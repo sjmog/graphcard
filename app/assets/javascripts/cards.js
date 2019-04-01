0756e5ef5bc35cd7445af94976eaa8f81da72cba
sjmog <s_morgan@me.com>
2019-04-01 22:36:04 +0100

    Allow the addition of new tags in the intuitive interface

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
   $('#card_form').submit((e) => {
-    const tags = $('.checkbox--tag:checked')
+    const checkboxTags = $('.checkbox--tag:checked')
                   .toArray()
                   .map(checkbox => $(checkbox).attr("name").match(/card\[(.+)\]/)[1])
 
-    $('#card_tag_list').val(tags);
+    const newTags = $('#card_new_tags').val().split(", ")
+
+    const tags = checkboxTags.concat(newTags);
+
+    $('#card_tag_list').val(tags.join(", "));
   });
 });
