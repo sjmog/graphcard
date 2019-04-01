3a2c7e8274f85df04e8470dbe291b53e608c72d6
sjmog <s_morgan@me.com>
2019-04-01 19:07:28 +0100

    Make README clearer and give seeds

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Graphcard
 
 Having another go at a connected flashcard thing.
+
+## Getting Started
+
+- Install Postgres.
+- Clone the repo.
+- `bundle`.
+- `rake db:setup` (you'll get my flashcards too)
+- `rails s`
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Card.create!([
+  {word: "regression", content: "predictive quantitative output.", last_viewed_at: nil, question: nil, tag_list: nil},
+  {word: "classification", content: "Predicting qualitative output", last_viewed_at: nil, question: nil, tag_list: nil},
+  {word: "dummy variable", content: "a K-level qualitative variable is represented by a vector of K binary variables\r\nor bits, only one of which is “on” at a time.", last_viewed_at: nil, question: nil, tag_list: nil},
+  {word: "", content: "\\(X_j\\)", last_viewed_at: nil, question: "If \\(X\\) is a vector, what is a component of that vector?", tag_list: nil}
+])
