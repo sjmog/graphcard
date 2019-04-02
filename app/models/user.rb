fc0ef807787274138003acfef260c9ebc24a41c8
sjmog <s_morgan@me.com>
2019-04-02 23:35:37 +0100

    Add intro SMS for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,27 @@ class User < ApplicationRecord
 
   has_many :cards
 
+  after_create :send_intro_sms_to_user, if: :phone_number_exists?
+
   def self.due_cards
     all.reject { |user| user.cards_due.empty? }
   end
 
   def cards_due
     cards.due
+  end
+
+  def phone_number_exists?
+    !!phone_number
+  end
+
+  def send_intro_sms_to_user
+    client = Twilio::REST::Client.new
+
+    client.messages.create({
+      from: ENV["TWILIO_PHONE_NUMBER"],
+      to:   phone_number,
+      body: "Welcome to Graphcard, #{name}! This is an automated message.\n\nI'll message you daily at 1300 with any cards to cover. You can create new cards via the web UI at https://graphcard.herokuapp.com, or you can reply to this message to create new cards. Simply add a delimiting '|' character between the question, the answer, and (optionally), a comma-separated list of tags. For instance:\n\nWhat's my name?|#{name}|personal, test\n\nHappy Graphcarding!"
+    })
   end
 end
--- lib/tasks/messages.rake
+++ lib/tasks/messages.rake
@@ -4,11 +4,13 @@ namespace :messages do
     client = Twilio::REST::Client.new
 
     User.due_cards.each do |user|
-      client.messages.create({
-        from: ENV["TWILIO_PHONE_NUMBER"],
-        to:   user.phone_number,
-        body: "You have #{ActionController::Base.helpers.pluralize(user.cards_due.count, 'card')} due!\n\nhttps://graphcard.herokuapp.com/learn"
-      })
+      if user.phone_number
+        client.messages.create({
+          from: ENV["TWILIO_PHONE_NUMBER"],
+          to:   user.phone_number,
+          body: "You have #{ActionController::Base.helpers.pluralize(user.cards_due.count, 'card')} due!\n\nhttps://graphcard.herokuapp.com/learn"
+        })
+      end
     end
   end
 
